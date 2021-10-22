4eb86acb4c922fb81d9cfaf8e70a662c4ed2c261
Vinogradoww <nik-rostov@mail.ru>
2021-10-22 14:25:57 +0300

    first commit

---
+++ Singleton/Singleton.py
@@ -0,0 +1,24 @@
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            instance = super().__call__(*args, **kwargs)
+            cls._instances[cls] = instance
+        return cls._instances[cls]
+
+
+class Singleton(metaclass=SingletonMeta):
+    def database(self):
+        
+        return 0
+
+
+
+if __name__ == "__main__":
+    s1 = Singleton()
+    s2 = Singleton()
+
+
+    print(s1)
+    print(s2)
