7103f1c24030cb614b0d2dd63a3d37326cfc5149
Vinogradoww <nik-rostov@mail.ru>
2021-10-22 16:51:15 +0300

    singleton

--- Singleton/Singleton.py
+++ Singleton/Singleton.py
@@ -1,3 +1,6 @@
+from pymongo import MongoClient
+
+
 class SingletonMeta(type):
     _instances = {}
 
@@ -8,17 +11,73 @@ class SingletonMeta(type):
         return cls._instances[cls]
 
 
-class Singleton(metaclass=SingletonMeta):
-    def database(self):
-        
-        return 0
+class Database(metaclass=SingletonMeta):
+    uri = ''
+    client = None
+    database = ''
 
+    def get_client(self, host: str = '127.0.0.1', port: int = 27017, username: str = '', password: str = ''):
+        if username:
+            uri = "mongodb://%s:%s@%s:%s" % (
+                username,
+                password,
+                host,
+                port
+            )
+        else:
+            uri = "mongodb://%s:%s" % (
+                host,
+                port
+            )
+        self.uri = uri
+        self.client = MongoClient(self.uri)
+        return self.client
+
+    def set_db(self, database: str = ''):
+        self.database = database
+        return self.database
+
+    def get_table(self, table: str = ''):
+        if not self.database:
+            return "Please select database (call method set_db())"
+        return self.client[self.database][table]
+
+    def _find(self, table: str = '', params=None):
+        if params is None:
+            params = {}
+        table = self.get_table(table)
+        if type(table) == str:
+            return table
+        return table.find(params)
+
+    def _find_one(self, table: str = '', params=None):
+        if params is None:
+            params = {}
+        table = self.get_table(table)
+        if type(table) == str:
+            return table
+        return table.find_one(params)
+
+    def _save(self, table: str = '', params=None):
+        if params is None:
+            params = {}
+        table = self.get_table(table)
+        if type(table) == str:
+            return table
+        return table.insert_one(params)
 
 
 if __name__ == "__main__":
-    s1 = Singleton()
-    s2 = Singleton()
+    s1 = Database()
+    s2 = Database()
+
+    print(s1, "\t", s1.get_client())
+    print(s2.client)
+
+    s1.set_db(database="mydb")
+    #res = s1._save("testtable", {"username": "admin", "password": "12345"})
+    #print(res)
+    #res = s1._find_one(table="testtable", params={"username": "admin"})
+    #print(res)
 
 
-    print(s1)
-    print(s2)
